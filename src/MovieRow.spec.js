2aa7403403d90e454ec9f3c2e03fb40a7affd89b
Josh Justice <me@codingitwrong.com>
2022-10-16 15:18:41 -0400

    Initial component implementation

---
+++ src/MovieRow.spec.js
@@ -0,0 +1,10 @@
+import {render, screen} from '@testing-library/react';
+import MovieRow from './MovieRow';
+
+describe('MovieRow', () => {
+  it('ADD YOUR FIRST TEST HERE', () => {
+    // add your test here
+  });
+
+  // add more it() tests here
+});
