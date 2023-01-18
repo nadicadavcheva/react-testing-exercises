062718ef8580587818b167962e91b32d2ec27d5e
Milos Kuzmic <milos.kuzmic@htecgroup.com>
2023-01-18 08:41:29 +0100

    Wrote 3 test for Excercises 1

--- src/MovieRow.spec.js
+++ src/MovieRow.spec.js
@@ -2,9 +2,21 @@ import {render, screen} from '@testing-library/react';
 import MovieRow from './MovieRow';
 
 describe('MovieRow', () => {
-  it('ADD YOUR FIRST TEST HERE', () => {
-    // add your test here
+  it('component will display text', () => {
+    const movie = {title: 'Black Panther: Wakanda Forever'};
+    render(<MovieRow movie={movie} />);
+    expect(screen.getByText(movie.title)).toBeVisible();
   });
-
-  // add more it() tests here
+  it('render icon if movie added today', () => {
+    const movie = {addedToday: true};
+    render(<MovieRow movie={movie} />);
+    expect(screen.getByTestId('move-added-today-icon')).toBeVisible();
+  });
+  it('do not render icon if the movie was not added today', () => {
+    const movie = {addedToday: false};
+    render(<MovieRow movie={movie} />);
+    expect(
+      screen.queryByTestId('move-added-today-icon'),
+    ).not.toBeInTheDocument();
+  });
 });
