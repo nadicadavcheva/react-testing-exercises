ffb621c619a6a9e2fe4277eaf53eaff1cad591c3
Milos Kuzmic <milos.kuzmic@htecgroup.com>
2023-03-27 09:32:28 +0200

    Refactored code for excercise 2

--- src/NewMovieForm.spec.js
+++ src/NewMovieForm.spec.js
@@ -1,27 +1,31 @@
-import { render, screen } from "@testing-library/react"
-import  userEvent  from "@testing-library/user-event"
-import NewMovieForm from "./NewMovieForm"
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NewMovieForm from './NewMovieForm';
 
-describe('NewMovieForm',()=>{
-    const movieTitle = 'Black Panter'
-    const user = userEvent.setup()
-    const createHandler = jest.fn().mockName('createHandler')
-    
-    it('component shown on page', ()=>{
-        render(<NewMovieForm onCreate={createHandler}/>)
-        expect(screen.getByLabelText('New movie title')).toBeVisible()
-    })
-    it('component reset input field after submit',async()=>{
-        render(<NewMovieForm onCreate={createHandler}/>)
-        await user.type(screen.getByPlaceholderText('New movie title'),movieTitle)
-        await user.click(screen.getByRole('button',{name:'Save'}))
-        expect(screen.getByPlaceholderText('New movie title')).toHaveValue('')
-    })
-    it('calls handleSubmit',async()=>{
-        render(<NewMovieForm onCreate={createHandler}/>)
-        await user.type(screen.getByPlaceholderText('New movie title'),movieTitle)
-        await user.click(screen.getByRole('button',{name:'Save'}))
-        expect(createHandler).toHaveBeenCalledWith(movieTitle)
+describe('NewMovieForm', () => {
+  let createHandler;
+  let movieTitle;
 
-    })
-})
+  const renderAndSave = async () => {
+    movieTitle = 'Black Panter';
+    const user = userEvent.setup();
+    createHandler = jest.fn().mockName('createHandler');
+
+    render(<NewMovieForm onCreate={createHandler} />);
+    await user.type(screen.getByPlaceholderText('New movie title'), movieTitle);
+    await user.click(screen.getByRole('button', {name: 'Save'}));
+  };
+
+  it('component should shown on page', async () => {
+    await renderAndSave();
+    expect(screen.getByLabelText('New movie title')).toBeVisible();
+  });
+  it('component should reset input field after submit', async () => {
+    await renderAndSave();
+    expect(screen.getByPlaceholderText('New movie title')).toHaveValue('');
+  });
+  it('component should call create handler ', async () => {
+    await renderAndSave();
+    expect(createHandler).toHaveBeenCalledWith(movieTitle);
+  });
+});
